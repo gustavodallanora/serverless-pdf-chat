d59a0ed0253b2d5838a1e3ea7faecb5de1188453
gdallanora <gustavo@compasso.com.br>
2024-12-11 11:28:08 -0300

    changes

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -4,7 +4,7 @@ import {
   ArrowLeftOnRectangleIcon,
   ChevronDownIcon,
 } from "@heroicons/react/24/outline";
-import { ChatBubbleLeftRightIcon } from "@heroicons/react/24/solid";
+import { DocumentMagnifyingGlassIcon } from "@heroicons/react/24/solid";
 
 interface NavigationProps {
   userInfo: any;
@@ -24,8 +24,8 @@ const Navigation: React.FC<NavigationProps> = ({
           to="/"
           className="inline-flex items-center self-center text-2xl font-semibold whitespace-nowrap text-white"
         >
-          <ChatBubbleLeftRightIcon className="w-6 h-6 mr-1.5" />
-          DocChat
+          <DocumentMagnifyingGlassIcon className="w-6 h-6 mr-1.5" />
+          Product Catalog Demo
         </Link>
         <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <div className="relative ml-3">
